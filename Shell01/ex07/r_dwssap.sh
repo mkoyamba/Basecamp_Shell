7f37736572c0df9520314123068dfe575b590e1c
mkoyamba <mkoyamba@student.s19.be>
2021-09-05 10:23:11 +0000

    ex7S1

--- Shell01/ex07/r_dwssap.sh
+++ Shell01/ex07/r_dwssap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 FT_LINE1=7
 FT_LINE2=8
-cat /etc/passwd | cut -d: -f1 | awk 'NR%2 == 0' | rev | sort -r | awk "NR >= $FT_LINE1 && NR <= $FT_LINE2" | tr '\n' ' ' | sed '$ s/ $/.\n/' | sed 's/ /, /g' | tr -d '\n'
+cat /etc/passwd | grep -v '#' | cut -d: -f1 | awk 'NR%2 == 0' | rev | sort -r | awk "NR >= $FT_LINE1 && NR <= $FT_LINE2" | tr '\n' ' ' | sed '$ s/ $/.\n/' | sed 's/ /, /g' | tr -d '\n'
